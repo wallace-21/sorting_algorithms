b39a05bbac90f4a6627b93c4379ffdb798f67081
Chideraa <chideraaozobu@gmail.com>
2023-11-19 23:18:28 +0200

    bubble and selection

---
+++ 0-bubble_sort.c
@@ -0,0 +1,28 @@
+#include <sort.h>
+/**
+ * bubble_sort - sorts array bubble algorithm
+ *
+ * @array: array to sort
+ * @size: size of array
+ * Return: sorted array
+ */
+
+void bubble_sort(int *array, size_t size)
+{
+	int swap;
+	size_t i, j;
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < (size - 1); j++)
+		{
+			if (array[j + 1] < array[j])
+			{
+				swap = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = swap;
+			}
+		}
+		print_array(array, size);
+	}
+}
---
+++ 2-selection_sort.c
@@ -0,0 +1,32 @@
+#include <sort.h>
+/**
+ * selection_sort - sorting selection algorithm
+ *
+ * @array: array to sort
+ * @size: size of the array
+ * Return: sorted array
+ **/
+
+void selection_sort(int *array, size_t size)
+{
+	int swap, smallest;
+	size_t i, j, index;
+
+	smallest = array[0];
+	for (i = 0; i < size, i++)
+	{
+		j = i;
+		for (j; j < size; j++)
+		{
+			if (array[j] < smallest)
+			{
+				smallest = array[j];
+				index = j;
+			}
+		}
+		swap = array[i];
+		array[i] = array[index];
+		array{index} = swap;
+		print_array(array, size);
+	}
+}
